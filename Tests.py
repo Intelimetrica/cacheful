8b0da452b73085d1c77a19cf3ba3588514fa0b17
Luis Guerra <lguerra@intelimetrica.com>
2016-07-07 17:34:48 -0500

    Reorganized folders and class names change

--- Tests.py
+++
@@ -1,6 +0,0 @@
-from Classes import cachestoreS
-
-if __name__ == '__main__':
-	columns = [["ID","INT"],["COUNT","INT"],["NOMBRE","TEXT"]]
-	c = cachestoresqlite("~/cacheful",columns)
-	print (type(c))
